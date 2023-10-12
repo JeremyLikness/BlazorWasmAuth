436fc8711137a64a22b9815d706e7fc9383ecc08
Jeremy Likness <jeremy@jeremylikness.com>
2023-10-12 10:35:21 -0700

    Add project files.

---
+++ BlazorWasmAuth/Identity/IAccountManagement.cs
@@ -0,0 +1,26 @@
+﻿using BlazorWasmAuth.Identity.Models;
+
+namespace BlazorWasmAuth.Identity
+{
+    /// <summary>
+    /// Account management services.
+    /// </summary>
+    public interface IAccountManagement
+    {
+        /// <summary>
+        /// Login service.
+        /// </summary>
+        /// <param name="email">User's email.</param>
+        /// <param name="password">User's password.</param>
+        /// <returns>The result of the request serialized to <see cref="FormResult"/>.</returns>
+        public Task<FormResult> LoginAsync(string email, string password);
+
+        /// <summary>
+        /// Registration service.
+        /// </summary>
+        /// <param name="email">User's email.</param>
+        /// <param name="password">User's password.</param>
+        /// <returns>The result of the request serialized to <see cref="FormResult"/>.</returns>
+        public Task<FormResult> RegisterAsync(string email, string password);
+    }
+}
